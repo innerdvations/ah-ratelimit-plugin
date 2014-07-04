89acd64243b2d4520bb7f4e31dd2a03797b0af11
Ben Irvin <ben@write-together.com>
2014-07-04 14:28:09 +0000

    fix postinstall script

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -6,6 +6,9 @@ var path = require('path');
 var localFile   = path.normalize(__dirname + '/../config/ratelimit.js');
 var projectFile = path.normalize(process.cwd() + '/../../config/plugins/ah-ratelimit-plugin.js');
 
+if(!fs.existsSync(process.cwd() + '/../../config/plugins')) {
+  fs.mkdirSync(process.cwd() + '/../../config/plugins');
+}
 if(!fs.existsSync(projectFile)){
   console.log("copying " + localFile + " to " + projectFile);
   fs.createReadStream(localFile).pipe(fs.createWriteStream(projectFile));
